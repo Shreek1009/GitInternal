b07c3da5ed9f23d240683c586d81220d6f9cb7da
Shreekara B <shreekarab5@gmail.com>
2024-02-21 11:44:07 +0530

    Commit

--- searching.py
+++ searching.py
@@ -1,8 +1,20 @@
-def search():
-    x=input('Enter the number')
-    if x==0:
-        print("Not found")
-        return 0
+def binarySearch(array, x, low, high):
+    if high >= low:
+        mid = low + (high - low)//2
+        if array[mid] == x:
+            return mid
+        elif array[mid] > x:
+            return binarySearch(array, x, low, mid-1)
+        else:
+            return binarySearch(array, x, mid + 1, high)
     else:
-        print("Keyword found")
-        return 1
+        return -1
+
+array = [3, 4, 5, 6, 7, 8, 9]
+x = 4
+result = binarySearch(array, x, 0, len(array)-1)
+
+if result != -1:
+    print("Element is present at index " + str(result))
+else:
+    print("Not found")
